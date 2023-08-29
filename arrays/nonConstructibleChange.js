768c3d30db397111fa4f0627de3c9cd392ed9bc5
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-08-29 11:16:03 -0700

    Add nonConstructibleChange in py and js. Also add autopep8 to vscode extensions.

---
+++ arrays/nonConstructibleChange.js
@@ -0,0 +1,20 @@
+// Non-constructible change
+
+// Given an array of positive integers, representing the values of coins in your possession, write a function that returns the minimum amount of change, (the minimum sum of money ) that you cannot create. The given coins can have any positive integer or value and aren’t necessarily unique i.e. you can have multiple coins of the same value.
+
+// For example, if you’re given coins equals 1 to 5 the minimum amount of change you can’t create is 4. If you’re given no coins, the minimum amount of change that you can’t create is 1.
+
+function nonConstructibleChange(array) {
+  let sorted = array.sort((a, b) => a - b);
+  let currentChange = 0;
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] > currentChange + 1) {
+      return currentChange + 1;
+    }
+    currentChange += sorted[i];
+  }
+}
+
+let coins = [5, 7, 1, 1, 2, 3, 22];
+
+console.log(nonConstructibleChange(coins));
