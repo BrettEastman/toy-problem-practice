b761eddcf43b370be87ec1090bef197fedba7460
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-09-26 20:58:41 -0700

    Add semordnilap in js

---
+++ strings/semordnilap.js
@@ -0,0 +1,20 @@
+// My solution 9/25/23
+function semordnilap(words) {
+  let copyWords = JSON.parse(JSON.stringify(words));
+
+  const resultArr = [];
+
+  while (copyWords.length) {
+    let currentWord = copyWords.shift();
+    let reverseWord = currentWord.split("").reverse().join("");
+    for (let i = 0; i < copyWords.length; i++) {
+      if (copyWords[i] === reverseWord) {
+        let duple = [];
+        duple.push(currentWord, reverseWord);
+        resultArr.push(duple);
+        copyWords.splice(i, 1);
+      }
+    }
+  }
+  return resultArr;
+}
