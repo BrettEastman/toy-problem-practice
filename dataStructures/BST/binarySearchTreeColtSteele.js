1bcc2dbde9f206af7385b5d2ddfed56d785bf9bd
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-12-15 10:50:39 -0800

    Add breadth-first search and depth-first search methods to binarySearchTreeColtSteele

--- dataStructures/BST/binarySearchTreeColtSteele.js
+++ dataStructures/BST/binarySearchTreeColtSteele.js
@@ -58,12 +58,81 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  BFS() {
+    let node = this.root;
+    let data = [];
+    let queue = [];
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
+
+  DFSPreOrder() {
+    let current = this.root;
+    let data = [];
+
+    function recurse(node) {
+      data.push(node.value);
+      // could also write this logic as:
+      // node.left && recurse(node.left)
+      // it's possibly more succinct, but really just means the same thing, so your choice
+      if (node.left) recurse(node.left);
+      if (node.right) recurse(node.right);
+    }
+
+    recurse(current);
+
+    return data;
+  }
+
+  DFSPostOrder() {
+    let current = this.root;
+    let data = [];
+
+    function recurse(node) {
+      if (node.left) recurse(node.left);
+      if (node.right) recurse(node.right);
+      data.push(node.value);
+    }
+
+    recurse(current);
+
+    return data;
+  }
+
+  DFSInOrder() {
+    let current = this.root;
+    let data = [];
+
+    function recurse(node) {
+      if (node.left) recurse(node.left);
+      data.push(node.value);
+      if (node.right) recurse(node.right);
+    }
+
+    recurse(current);
+
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(2);
-// console.log(tree);
-console.log(tree.find(10));
-// tree.find(2);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.BFS()); // [ 10, 6, 15, 3, 8, 20 ]
+console.log(tree.DFSPreOrder()); // [ 10, 6, 3, 8, 15, 20 ]
+console.log(tree.DFSPostOrder()); // [ 3, 8, 6, 20, 15, 10 ]
+console.log(tree.DFSInOrder()); // [ 3, 6, 8, 10, 15, 20 ]
