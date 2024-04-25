66cf49bfb77e03c86387e37e95960417737698eb
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-04-25 10:40:01 -0700

    Rewrite comments in dijkstrasAlgoexpert.js

--- famousAlgorithms/dijkstrasAlgoexpert.js
+++ famousAlgorithms/dijkstrasAlgoexpert.js
@@ -1,48 +1,50 @@
 function dijkstrasAlgorithm(start, edges) {
   const length = edges.length;
+  // create the initial minDistances array and fill it with Infinity
   let minDistances = new Array(length).fill(Infinity);
+  // but, set the start index of minDistances to 0
   minDistances[start] = 0;
 
   let visited = new Set();
 
-  // while visted size is less than length
   while (visited.size < length) {
     // create a helper function that takes in the minDistances array and visited set, to return the vertex with the current minimum distance
-    // the function should return an array with the vertex and its min distance
+    // what we want this function to return is an array with the vertex and its min distance
     let arr = getVertexWithMinimumDistance(minDistances, visited);
-    // create vertex var = arr[0] and currentMinDistance var = arr[1]
+
+    // Now we deconstruct the array, assigning variables to the vertex and its min distance
     let vertex = arr[0];
     let currentMinDistance = arr[1];
-    // if currentMinDistance === 'inf' then break out of the loop
+
+    // if currentMinDistance is Infinty, then there is no path to the vertex
     if (currentMinDistance === Infinity) {
       break;
     }
-    // add the vertex to the visited set
+
     visited.add(vertex);
+    // all the directed edges from the current vertex
     let currentEdges = edges[vertex];
 
-    // for loop through that vertex's edges
     for (let edge of currentEdges) {
-      // for each one, destination = edge[0] and distanceToDestination = edge[1]
+      // for each two-item array, deconstruct the array to get the destination and the distance to the destination
       const destination = edge[0];
       const distanceToDestination = edge[1];
-      // if destination already visited, skip with continue
+
+      // if destination already visited, we don't need to visit it again
       if (visited.has(destination)) {
         continue;
       }
-      // create newPathDistance var = currentMinDistance + distanceToDestination
+
       const newPathDistance = currentMinDistance + distanceToDestination;
-      // create currentDestinationDistance = minDistances[destination]
       const currentDestinationDistance = minDistances[destination];
 
-      // if newPathDistance is less than currentDestinationDistance
-      // then minDistances[destination] = newPathDistance
+      // if newPathDistance is less than currentDestinationDistance, update the minDistances array
       if (newPathDistance < currentDestinationDistance) {
         minDistances[destination] = newPathDistance;
       }
     }
   }
-  // return minDistances array, but change and "inf" to -1
+  // return minDistances array, but change any Infinity's to -1
   for (let j = 0; j < minDistances.length; j++) {
     if (minDistances[j] === Infinity) {
       minDistances[j] = -1;
@@ -52,15 +54,14 @@ function dijkstrasAlgorithm(start, edges) {
 }
 
 function getVertexWithMinimumDistance(minDistArr, visitedSet) {
-  // create result arr for vertex and min dist with result[0] set to null and result[1] set to Infinity
+  // create initial result arr for vertex and min dist with result[0] set to null and result[1] set to Infinity
   let result = [null, Infinity];
-  // iterate over the minDistArr
+
   for (let i = 0; i < minDistArr.length; i++) {
-    // if the current index (vertex) is in visited,
+    // Again, if already visited, we don't need to visit it again
     if (visitedSet.has(i)) {
       continue;
     }
-    // if minDistArr[i] is less than or equal to result[1]
     if (minDistArr[i] <= result[1]) {
       result[0] = i;
       result[1] = minDistArr[i];
